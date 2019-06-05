ee265d1e7c5a8203c0ec526a3eccc3ea4e16f94d
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2019-06-05 19:46:02 +0300

    refactor(handler) no need to to inherit from BasePlugin anymore

--- kong/plugins/kubernetes-sidecar-injector/handler.lua
+++ kong/plugins/kubernetes-sidecar-injector/handler.lua
@@ -1,14 +1,8 @@
-local BasePlugin = require "kong.plugins.base_plugin"
+local Handler = {
+  VERSION = "0.1.1",
+  -- priority doesn't matter, just need to pick something unique for kong tests
+  PRIORITY = 1006,
+}
 
-local Handler = BasePlugin:extend()
-
-Handler.VERSION = "0.1.1"
-
--- priority doesn't matter, just need to pick something unique for kong tests
-Handler.PRIORITY = 1006
-
-function Handler:new()
-  Handler.super.new(self, "kubernetes-sidecar-injector")
-end
 
 return Handler
